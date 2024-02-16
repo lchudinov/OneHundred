b754a05e9a0d27208284228ac9c3252d33f75383
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-02-16 08:35:27 +0500

    Initial commit

---
+++ oneHundred.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+digits = [1, 2, 3, 4, 5, 6, 7]
+solutions = []
+one_hundred = 100
+
+for i in range(7):
+  number = digits[i]
+  rest = [x for j, x in enumerate(digits) if j!=i]
+  for perm in permutations(rest, 6):
+    result = number + (perm[0] * 10 + perm[1]) + (perm[2] * 10 + perm[3]) + (perm[4] * 10 + perm[5])
+    if result == one_hundred:
+      solutions.append((number, perm[0] * 10 + perm[1], perm[2] * 10 + perm[3], perm[4] * 10 + perm[5]))
+
+print ("Solutions found:", len(solutions))
+print ("Are solutions unique?", len(solutions) == len(set(solutions)))
+print ("Are solutions correct?", all(one_hundred == sum(solution) for solution in solutions))
+for solution in solutions:
+  print (solution)
